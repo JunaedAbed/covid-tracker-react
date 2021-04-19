2624d70e476f5db2a274670a43ee507f3e6d5b54
Junaed Abed <junaedccpc17@gmail.com>
2021-04-20 04:31:56 +0600

    cards implemented

---
+++ src/components/InfoBox.js
@@ -0,0 +1,29 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+import CountUp from "react-countup";
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography
+          className="infoBox__title"
+          color="textSecondary"
+          gutterBottom
+        >
+          {title}
+        </Typography>
+
+        <h2 className="infoBox__cases">
+          <CountUp start={0} end={cases} duration={2} separator="," />
+        </h2>
+
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
